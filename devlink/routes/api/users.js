c594ed71343b3a6d0700e689e3d560b84b158bfa
emmakuen <emmaku@protonmail.com>
2022-01-13 18:26:50 +0900

    add user registration

--- devlink/routes/api/users.js
+++ devlink/routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
+const { check, validationResult, body } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc    Register User
@@ -15,13 +19,46 @@ const validation = [
   ).isLength({ min: 6 }),
 ];
 
-router.post("/", validation, (req, res) => {
+router.post("/", validation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+  const { name, email, password } = req.body;
 
-  res.send("User Route");
+  try {
+    // See if user exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+    }
+    // Send user's gravatar
+    const avatar = gravatar.url(email, {
+      s: "200",
+      r: "g",
+      d: "mp",
+    });
+
+    user = new User({ name, email, avatar, password });
+
+    // Encrypt password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    // Return jsonwebtoken
+
+    res.send("User Registered");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
+
+//// ==== GRAVATAR OPTIONS ==== //
+//      s: "200", // size
+//      r: "g", // rating - appropriate for all audience
+//      d: "mp", // default - user icon (mp: mystery person)
