de2356a4c115026f1f158383c3e15f17493d8c5a
emmakuen <emmaku@protonmail.com>
2022-01-13 16:49:04 +0900

    set up routes

--- devlink/server.js
+++ devlink/server.js
@@ -6,9 +6,13 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.get("/", (req, res) => res.send("hello"));
+
+// Define Routes
+app.use(`/api/users`, require("./routes/api/users"));
+app.use(`/api/auth`, require("./routes/api/auth"));
+app.use(`/api/profile`, require("./routes/api/profile"));
+app.use(`/api/posts`, require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
