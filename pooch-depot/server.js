97fe4b6174bcb9cf8109ec58a2647e0defc77843
emmakuen <emmaku@protonmail.com>
2022-01-24 18:58:48 +0900

    connect to db

--- pooch-depot/server.js
+++ pooch-depot/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const constants = require("./config/constants");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) => res.send("API running..."));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
+app.listen(constants.PORT, () =>
+  console.log(`Server started on port ${constants.PORT}.`)
+);
